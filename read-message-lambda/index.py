00e81f32833ded6a410cd744873bfd9e78f8c838
Punit Lad <punitlad@gmail.com>
2023-03-20 17:08:21 -0400

    fetch logger

--- read-message-lambda/index.py
+++ read-message-lambda/index.py
@@ -4,9 +4,10 @@ import logging
 
 def lambda_handler(event, context):
     logging.getLogger().addHandler(logging.StreamHandler())
+    logger = logging.getLogger("read-message-lambda")
 
-    logging.info("Triggered from sqs message")
-    logging.info(event["Records"][0]["body"])
+    logger.info("Triggered from sqs message")
+    logger.info(event["Records"][0]["body"])
     return {
         'statusCode': 200,
         'body': json.dumps("Goodbye from Lambda!")
--- send-message-lambda/index.py
+++ send-message-lambda/index.py
@@ -6,10 +6,11 @@ import logging
 
 def lambda_handler(event, context):
     logging.getLogger().addHandler(logging.StreamHandler())
+    logger = logging.getLogger("send-message-lambda")
 
     sqs = boto3.client('sqs')
     send_message_id = uuid.uuid4()
-    logging.info("ID: {id}".format(id=send_message_id))
+    logger.info("ID: {id}".format(id=send_message_id))
     sqs.send_message(
         QueueUrl=os.environ['SQS_URL'],
         MessageBody="Send Message ID: {id}".format(id=send_message_id)
