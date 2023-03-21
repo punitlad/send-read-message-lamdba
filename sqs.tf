f881238c1baafd5080a68658fc220422ee537589
Punit Lad <punitlad@gmail.com>
2023-03-20 22:42:13 -0400

    remove kms key as these lambdas are purely for test purposes not production

--- sqs.tf
+++ sqs.tf
@@ -1,12 +1,4 @@
 resource "aws_sqs_queue" "message_queue" {
+  #checkov:skip=CKV_AWS_27: This is just a test queue; not for production usage
   name = "message-queue"
-  kms_master_key_id                 = aws_kms_key.sqs_kms_key.arn
-  kms_data_key_reuse_period_seconds = 300
-}
-
-resource "aws_kms_key" "sqs_kms_key" {
-  description             = "sqs-kms-key"
-  deletion_window_in_days = 7
-  is_enabled              = true
-  enable_key_rotation    = true
 }
